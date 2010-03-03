ae711047044a2dd69bb6cc6f276de169cbd69928
Barry Kaplan <barry@systeminsights.com>
2010-03-02 16:06:36 -0800

    Fully exacutable scala internal-dsl spike

--- drools-scala-dsl/src/test/scala/memelet/drools/scala/dsl/SpikeRules.scala
+++ drools-scala-dsl/src/test/scala/memelet/drools/scala/dsl/SpikeRules.scala
@@ -2,97 +2,96 @@ package memelet.drools.scala.dsl
 
 object Rules {
 
-  case class AgendaGroup(name: String)
-  implicit def stringToSomeAgendaGroup(name: String): Option[AgendaGroup] = Some(AgendaGroup(name))
-  implicit def agendaGroupToSomeAgendaGroup(agendaGroup: AgendaGroup): Option[AgendaGroup] = Some(agendaGroup)
-
-  case class Rule(
-          name: String,
-          salience: Int = 0,
-          agendaGroup: Option[AgendaGroup] = None,
-          ruleflowGroup: Option[String] = None,
-          lockOnActive: Boolean = false,
-          noLoop: Boolean = false
-          )
-  {
-    def then (consequence: => Unit) { consequence }
-  }
-
-  class Evaluator
-
-  case class Expression(property: Symbol, matcher: Evaluator)
-
-  class PatternResult[T: Manifest] {
-    def value: T = throw new UnsupportedOperationException
-    def apply: T = value
-  }
-  implicit def patternResultToValue[T: Manifest](patternResult: PatternResult[T]) = patternResult.value
-
-  abstract class PatternDsl[T: Manifest] {
-    def apply(expressions: Expression*): PatternResult[T] = {
-      val result = new PatternResult[T]
-      result
-    }
-  }
-
-  class SymbolDsl(symbol: Symbol) {
-    def is_==(value: Any) = Expression(symbol, new Evaluator)
-    def is_!=(value: Any) = Expression(symbol, new Evaluator)
-    def is_eq(result: PatternResult[_]) = Expression(symbol, new Evaluator)
-    def is_same = is_eq _
-    def contains(values: Set[Any]) = Expression(symbol, new Evaluator)
-  }
-  implicit def symbolDsl(symbol: Symbol): SymbolDsl = new SymbolDsl(symbol)
-}
-
-
-
-class SpikeRules {
-
-  import memelet.drools.scala.dsl.Rules._
-
-  class FactOne
-  class FactTwo {
-    val things: Seq[String] = Seq("one", "two")
-  }
-  class FactThree
-
-  object AgendaXXX extends AgendaGroup("XXX")
-  object AgendaYYY extends AgendaGroup("XXX")
-
-  object FactOne extends PatternDsl[FactOne]
-  object FactTwo extends PatternDsl[FactTwo]
-  object FactThree extends PatternDsl[FactThree]
-
-  private def doStuff(f1: FactOne, f2: FactTwo) { /*...*/ }
-
-  //-----------------------------------------------------
-  object RuleX extends Rule (
-    name = "ruleX",
-    agendaGroup = AgendaXXX)
-  {
-    val f1 = FactOne('foo is_== "bar", 'bla is_!= "blaz")
-    val f2 = FactTwo('f1 is_same f1)
-    FactThree('blazz contains Set("X", "Y"))
-
-    then {
-      f2.things.filter(_ == "bar")
-    }
-  }
-  //-----------------------------------------------------
-
-  object RuleY extends Rule (
-    name = "ruleY",
-    agendaGroup = "YYY")
-  {
-    val f1 = FactOne('foo is_== "bar", 'bla is_!= "blaz")
-    val f2 = FactTwo('f1 is_same f1)
-    FactThree('blazz contains Set("X", "Y"))
-
-    then {
-      doStuff(f1, f2)
-    }
-
-  }
+//  case class AgendaGroup(name: String)
+//  implicit def stringToSomeAgendaGroup(name: String): Option[AgendaGroup] = Some(AgendaGroup(name))
+//  implicit def agendaGroupToSomeAgendaGroup(agendaGroup: AgendaGroup): Option[AgendaGroup] = Some(agendaGroup)
+//
+//  case class Rule(
+//          name: String,
+//          salience: Int = 0,
+//          agendaGroup: Option[AgendaGroup] = None,
+//          ruleflowGroup: Option[String] = None,
+//          lockOnActive: Boolean = false,
+//          noLoop: Boolean = false
+//          )
+//  {
+//    def then (consequence: => Unit) { consequence }
+//  }
+//
+//  class DslEvaluator
+//
+//  case class DslConstraint(property: Symbol, matcher: DslEvaluator)
+//
+//  class DslDeclaration[T: Manifest] {
+//    def value: T = throw new UnsupportedOperationException
+//    def apply: T = value
+//  }
+//  implicit def dslDeclarationToValue[T: Manifest](dslDeclaration: DslDeclaration[T]) = dslDeclaration.value
+//
+//  abstract class DslPattern[T: Manifest] {
+//    def apply(constraints: DslConstraint*): DslDeclaration[T] = {
+//      val result = new DslDeclaration[T]
+//      result
+//    }
+//  }
+//
+//  class SymbolDsl(symbol: Symbol) {
+//    def ~==(value: Any) = DslConstraint(symbol, new DslEvaluator)
+//    def ~!=(value: Any) = DslConstraint(symbol, new DslEvaluator)
+//    def ~==(result: DslDeclaration[_]) = DslConstraint(symbol, new DslEvaluator)
+//    def contains(values: Set[Any]) = DslConstraint(symbol, new DslEvaluator)
+//  }
+//  implicit def symbolDsl(symbol: Symbol): SymbolDsl = new SymbolDsl(symbol)
+//}
+//
+//
+//
+//class SpikeRules {
+//
+//  import memelet.drools.scala.dsl.Rules._
+//
+//  class FactOne
+//  class FactTwo {
+//    val things: Seq[String] = Seq("one", "two")
+//  }
+//  class FactThree
+//
+//  object AgendaXXX extends AgendaGroup("XXX")
+//  object AgendaYYY extends AgendaGroup("XXX")
+//
+//  object FactOne extends DslPattern[FactOne]
+//  object FactTwo extends DslPattern[FactTwo]
+//  object FactThree extends DslPattern[FactThree]
+//
+//  private def doStuff(f1: FactOne, f2: FactTwo) { /*...*/ }
+//
+//  //-----------------------------------------------------
+//  object RuleX extends Rule (
+//    name = "ruleX",
+//    agendaGroup = AgendaXXX)
+//  {
+//    val f1 = FactOne('foo ~== "bar", 'bla ~!= "blaz")
+//    val f2 = FactTwo('f1 ~== f1)
+//    FactThree('blazz contains Set("X", "Y"))
+//
+//    then {
+//      f2.things.filter(_ == "bar")
+//    }
+//  }
+//  //-----------------------------------------------------
+//
+//  object RuleY extends Rule (
+//    name = "ruleY",
+//    agendaGroup = "YYY")
+//  {
+//    val f1 = FactOne('foo ~== "bar", 'bla ~!= "blaz")
+//    val f2 = FactTwo('f1 ~== f1)
+//    FactThree('blazz contains Set("X", "Y"))
+//
+//    then {
+//      doStuff(f1, f2)
+//    }
+//
+//  }
 
 }
--- drools-scala-dsl/src/test/scala/memelet/drools/scala/dsl/SpikeSpec.scala
+++ drools-scala-dsl/src/test/scala/memelet/drools/scala/dsl/SpikeSpec.scala
@@ -12,14 +12,15 @@ import field.ObjectFieldImpl
 import org.drools.spi._
 import org.drools.reteoo.builder.ReteooRuleBuilder
 import memelet.drools.scala.{DroolsDebug, RichDrools, DroolsFixture}
-import org.drools.reteoo.{ReteooStatefulSession, ReteooBuilder, ReteooRuleBase}
 import org.drools.impl.{KnowledgeBaseImpl, StatefulKnowledgeSessionImpl}
 import org.drools.runtime.StatefulKnowledgeSession
 import org.drools.{KnowledgeBase, KnowledgeBaseFactory, WorkingMemory, RuleBaseConfiguration}
-import org.drools.common.{InternalRuleBase, InternalWorkingMemory}
+import org.drools.reteoo.{LeftTuple, ReteooStatefulSession, ReteooBuilder, ReteooRuleBase}
+import org.drools.common.{InternalFactHandle, InternalRuleBase, InternalWorkingMemory}
+import util.DynamicVariable
 
-case class FactOne(name: String)
-case class FactTwo(name: String, f: FactOne)
+case class FactOneX(name: String)
+case class FactTwoX(name: String, f: FactOneX)
 
 class SpikeSpec extends SpecsMatchers with Mockito {
 
@@ -28,16 +29,131 @@ class SpikeSpec extends SpecsMatchers with Mockito {
     import drools._
     import RichDrools._
 
-    val f1_1 = FactOne("f1_1")
-    val f1_2 = FactOne("f1_2")
-    val f2_1 = FactTwo("f2_1", f1_1)
+    val f1_1 = FactOneX("f1_1")
+    val f1_2 = FactOneX("f1_2")
+    val f2_1 = FactTwoX("f2_1", f1_1)
     session insert f1_1
     session insert f1_2
     session insert f2_1
     session fireAllRules
   }
 
-  @Test def createKBase {
+  //--------------------------------------------------------------------------------------------------------------------
+
+  val classFieldAccessorStore = new ClassFieldAccessorStore
+  classFieldAccessorStore.setClassFieldAccessorCache(new ClassFieldAccessorCache(this.getClass.getClassLoader))
+  val evaluatorRegistry = new EvaluatorRegistry
+
+  case class DslRule(
+          name: String,
+          salience: Int = 0,
+          agendaGroup: Option[AgendaGroup] = None,
+          ruleflowGroup: Option[String] = None,
+          lockOnActive: Boolean = false,
+          noLoop: Boolean = false
+          )
+      extends org.drools.rule.Rule(name)
+  { rule =>
+
+    private var declarationIndex = 0
+    def nextDeclarationIndex = {
+      declarationIndex += 1
+      declarationIndex
+    }
+
+    implicit val self: DslRule = this
+
+    private val consequenceFacts = new DynamicVariable[Array[InternalFactHandle]](null)
+    def facts = consequenceFacts.value
+
+    class DslConsequence(f: => Unit) extends Consequence {
+      def evaluate(knowledgeHelper: KnowledgeHelper, workingMemory: WorkingMemory) = {
+        consequenceFacts.withValue(knowledgeHelper.getTuple.asInstanceOf[LeftTuple].toFactHandles) {
+          f
+        }
+      }
+    }
+
+    implicit def dslDeclarationToValue[T](dslDeclaration: DslDeclaration[T]) = dslDeclaration.value
+    
+    def then (f: => Unit) { this.setConsequence(new DslConsequence(f)) }
+  }
+
+  class DslDeclaration[T](val underlying: Declaration) {
+    def value(implicit rule: DslRule): T = {
+      val handle = rule.facts(underlying.getPattern.getOffset)
+      handle.getObject.asInstanceOf[T]
+    }
+  }
+
+  class DslPattern[T: Manifest] {
+    val objectType = new ClassObjectType(manifest[T].erasure)
+    def classType = objectType.getClassType
+    var underlying: Pattern = _
+    var declaration: DslDeclaration[T] = _
+
+    def apply(constraints: (DslPattern[T] => Constraint)*)(implicit rule: DslRule): DslDeclaration[T] = {
+      val index = rule.nextDeclarationIndex
+      underlying = new Pattern(index, index, objectType, index.toString)
+      declaration = new DslDeclaration(underlying.getDeclaration)
+
+      constraints.foreach{ c =>
+        underlying.addConstraint(c(this))
+      }
+
+      rule.addPattern(underlying)
+
+      declaration
+    }
+  }
+
+  def LiteralConstraint[T](dslPattern: DslPattern[T])(property: Symbol, evaluator: Evaluator, value: Any): Constraint = {
+    val reader: ClassFieldReader = classFieldAccessorStore.getReader(dslPattern.classType, property.name, null)
+    val restriction: LiteralRestriction = new LiteralRestriction(new ObjectFieldImpl(value), evaluator, reader)
+    val constraint = new LiteralConstraint(reader, restriction)
+    constraint
+  }
+
+  def VariableConstraint[T](dslPattern: DslPattern[T])(property: Symbol, evaluator: Evaluator, declaration: Declaration): Constraint = {
+    val reader: ClassFieldReader = classFieldAccessorStore.getReader(dslPattern.classType, property.name, null)
+    val restriction: VariableRestriction = new VariableRestriction(reader, declaration, evaluator)
+    val constraint = new VariableConstraint(reader, restriction)
+    constraint
+  }
+
+  class DslSymbol(symbol: Symbol) {
+    def ~==(value: String) = LiteralConstraint(_:DslPattern[_])(symbol, evaluatorRegistry.getEvaluator(ValueType.OBJECT_TYPE, "==", false, null), value)
+    def ~!=(value: String) = LiteralConstraint(_:DslPattern[_])(symbol, evaluatorRegistry.getEvaluator(ValueType.OBJECT_TYPE, "!=", false, null), value)
+    def ~==(declaration: DslDeclaration[_]) = VariableConstraint(_:DslPattern[_])(symbol, evaluatorRegistry.getEvaluator(ValueType.OBJECT_TYPE, "==", false, null), declaration.underlying)
+  }
+  implicit def dslSymbol(symbol: Symbol): DslSymbol = new DslSymbol(symbol)
+
+
+
+  case class FactOne(name: String)
+  case class FactTwo(name: String, f: FactOne) {
+      val things: Seq[String] = Seq("one", "two")
+    }
+
+  object FactOne extends DslPattern[FactOne]
+  object FactTwo extends DslPattern[FactTwo]
+
+  private def doStuff(f1: FactOne, f2: FactTwo) { /*...*/ }
+
+  //-----------------------------------------------------
+  class RuleX extends DslRule (
+    name = "ruleX")
+  {
+    val f1 = FactOne('name ~== "f1_1")
+    val f2 = FactTwo('f ~== f1)
+
+    then {
+      f2.things.filter(_ == "bar")
+    }
+  }
+  //-----------------------------------------------------
+
+  @Test def createFromDsl {
 
     val kbase: KnowledgeBase = KnowledgeBaseFactory.newKnowledgeBase()
     val rbase = kbase.asInstanceOf[KnowledgeBaseImpl].getRuleBase.asInstanceOf[InternalRuleBase]
@@ -45,71 +161,58 @@ class SpikeSpec extends SpecsMatchers with Mockito {
     val reteooBuilder = rbase.getReteooBuilder
     val ruleBuilder = new ReteooRuleBuilder
 
-    val classFieldAccessorCache = new ClassFieldAccessorCache(this.getClass.getClassLoader)
-    val classFieldAccessorStore = new ClassFieldAccessorStore
-    classFieldAccessorStore.setClassFieldAccessorCache(classFieldAccessorCache)
-    val evaluatorRegistry = new EvaluatorRegistry
+    val ruleX = new RuleX
+    ruleBuilder.addRule(ruleX, rbase, reteooBuilder.getIdGenerator)
 
-    val factOneObjectType = new ClassObjectType(classOf[FactOne])
-    val factTwoObjectType = new ClassObjectType(classOf[FactTwo])
+    val ksession = kbase.newStatefulKnowledgeSession()
+    DroolsDebug.debugWorkingMemory(ksession)
+    DroolsDebug.debugAgenda(ksession)
 
-    val rule_1 = {
-      val rule = new Rule("rule_1")
-      val lhsroot = GroupElementFactory.newAndInstance
+    val f1_1 = FactOne("f1_1")
+    val f2_1 = FactTwo("f2_1", f1_1)
+    ksession insert f1_1
+    ksession insert f2_1
+    ksession fireAllRules
+    
+  }
 
-      val pattern_1 = {
-        val pattern = new Pattern(0, factOneObjectType, "f1_1")
-        val literalConstraint__FactOne_name = {
-          val reader: ClassFieldReader = classFieldAccessorStore.getReader(classOf[FactOne], "name", null)
-          val evaluator: Evaluator = evaluatorRegistry.getEvaluator(ValueType.STRING_TYPE, "==", false, null)
-          val value: FieldValue = new ObjectFieldImpl("f1_1")
-          val restriction: LiteralRestriction = new LiteralRestriction(value, evaluator, reader)
-          val constraint = new LiteralConstraint(reader, restriction)
-          constraint
+  @Test def createKBase {
+
+    case class FactOne(name: String)
+    case class FactTwo(name: String, f: FactOne)
+
+    //---- ---- ---
+
+    val kbase: KnowledgeBase = KnowledgeBaseFactory.newKnowledgeBase()
+    val rbase = kbase.asInstanceOf[KnowledgeBaseImpl].getRuleBase.asInstanceOf[InternalRuleBase]
+
+    val reteooBuilder = rbase.getReteooBuilder
+    val ruleBuilder = new ReteooRuleBuilder
+
+    val rule_1 = new DslRule("rule_1") {
+        val pattern_1 = new DslPattern[FactOne]
+        val pattern_1_decl = pattern_1.apply(
+          LiteralConstraint(_:DslPattern[_])('name, evaluatorRegistry.getEvaluator(ValueType.OBJECT_TYPE, "==", false, null), "f1_1")
+        )
+        addPattern(pattern_1.underlying)
+
+        val pattern_2 = new DslPattern[FactTwo]
+        val pattern_2_decl = pattern_2.apply(
+          LiteralConstraint(_:DslPattern[_])('name, evaluatorRegistry.getEvaluator(ValueType.OBJECT_TYPE, "==", false, null), "f2_1"),
+          VariableConstraint(_:DslPattern[_])('f, evaluatorRegistry.getEvaluator(ValueType.OBJECT_TYPE, "==", false, null), pattern_1_decl.underlying)
+        )
+        addPattern(pattern_2.underlying)
+
+        val consequence = new Consequence {
+          def evaluate(knowledgeHelper: KnowledgeHelper, workingMemory: WorkingMemory) = {
+            println("*** consequence")
+          }
         }
-        pattern.addConstraint(literalConstraint__FactOne_name)
-        pattern
-      }
-      lhsroot.addChild(pattern_1)
-
-      val pattern_2 = {
-        val pattern = new Pattern(0, factTwoObjectType, "f2_1")
-        val literalConstraint__FactTwo_name = {
-          val reader: ClassFieldReader = classFieldAccessorStore.getReader(classOf[FactTwo], "name", null)
-          val evaluator: Evaluator = evaluatorRegistry.getEvaluator(ValueType.STRING_TYPE, "==", false, null)
-          val value: FieldValue = new ObjectFieldImpl("f2_1")
-          val restriction: LiteralRestriction = new LiteralRestriction(value, evaluator, reader)
-          val constraint = new LiteralConstraint(reader, restriction)
-          constraint
-        }
-        pattern.addConstraint(literalConstraint__FactTwo_name)
-
-        val variableConstraint__FactTwo_f = {
-          val reader: ClassFieldReader = classFieldAccessorStore.getReader(classOf[FactTwo], "f", null)
-          val evaluator: Evaluator = evaluatorRegistry.getEvaluator(ValueType.OBJECT_TYPE, "==", false, null)
-          val patternExtractor = new PatternExtractor(factTwoObjectType)
-          val restriction: VariableRestriction = new VariableRestriction(reader, pattern_1.getDeclaration, evaluator)
-          val constraint = new VariableConstraint(reader, restriction)
-          constraint
-        }
-        pattern.addConstraint(variableConstraint__FactTwo_f)
-
-        pattern
-      }
-      lhsroot.addChild(pattern_2)
-      
-      rule.setLhs(lhsroot)
-
-      val consequence = new Consequence {
-        def evaluate(knowledgeHelper: KnowledgeHelper, workingMemory: WorkingMemory) = {
-          println("*** consequence")
-        }
-      }
-      rule.setConsequence(consequence)
-
-      rule
+        setConsequence(consequence)
     }
     ruleBuilder.addRule(rule_1, rbase, reteooBuilder.getIdGenerator)
+
+    //----
 
     val ksession = kbase.newStatefulKnowledgeSession()
     DroolsDebug.debugWorkingMemory(ksession)
@@ -122,5 +225,7 @@ class SpikeSpec extends SpecsMatchers with Mockito {
     ksession fireAllRules
 
   }
-  
+
+
 }
+
