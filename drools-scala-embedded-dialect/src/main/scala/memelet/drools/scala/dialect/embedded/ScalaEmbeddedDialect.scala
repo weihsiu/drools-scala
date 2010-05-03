e1073b4f4c2832184f9ea9f6291bb21617f69ddb
Barry Kaplan <barry@systeminsights.com>
2010-05-03 11:07:33 -0700

    Moved all dialect code into embedded-dialect module since I plan to implemented a DRL based dialect as well.

--- drools-scala-embedded-dialect/src/main/scala/memelet/drools/scala/dialect/embedded/ScalaEmbeddedDialect.scala
+++ drools-scala-embedded-dialect/src/main/scala/memelet/drools/scala/dialect/embedded/ScalaEmbeddedDialect.scala
@@ -1,12 +1,12 @@
-package memelet.drools.scala.dialect
+package memelet.drools.scala.dialect.embedded
 
 import org.drools.rule.builder.dialect.mvel.MVELDialect
 import org.drools.rule.builder.ConsequenceBuilder
 import org.drools.compiler.{PackageBuilder, PackageRegistry}
 
-class ScalaDialect(builder: PackageBuilder,
-                   pkgRegistry: PackageRegistry,
-                   pkg: org.drools.rule.Package)
+class ScalaEmbeddedDialect(builder: PackageBuilder,
+                           pkgRegistry: PackageRegistry,
+                           pkg: org.drools.rule.Package)
         extends MVELDialect(builder, pkgRegistry, pkg, "scala") {
 
   override def getConsequenceBuilder: ConsequenceBuilder = ScalaConsequenceBuilder
--- drools-scala-embedded-dialect/src/main/scala/memelet/drools/scala/dialect/embedded/ScalaEmbeddedDialectConfiguration.scala
+++ drools-scala-embedded-dialect/src/main/scala/memelet/drools/scala/dialect/embedded/ScalaEmbeddedDialectConfiguration.scala
@@ -1,12 +1,12 @@
-package memelet.drools.scala.dialect
+package memelet.drools.scala.dialect.embedded
 
 import org.drools.rule.builder.dialect.mvel.MVELDialectConfiguration
 import org.drools.compiler.{PackageBuilderConfiguration, Dialect, PackageRegistry, PackageBuilder}
 
-class ScalaDialectConfiguration extends MVELDialectConfiguration {
+class ScalaEmbeddedDialectConfiguration extends MVELDialectConfiguration {
 
   override def newDialect(packageBuilder: PackageBuilder, pkgRegistry: PackageRegistry, pkg: org.drools.rule.Package): Dialect = {
-    return new ScalaDialect(packageBuilder, pkgRegistry, pkg)
+    return new ScalaEmbeddedDialect(packageBuilder, pkgRegistry, pkg)
   }
 
   //TODO Delete these if we end up extending MVEL yet don't change the bahavior
